c0e3dd9f8a44a14f767999457b41cb6a9fa81c8f
SeopBong <gkstjq309@gmail.com>
2023-09-07 11:20:48 +0900

    useraddAppDone

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -3,18 +3,27 @@ import Card from '../components/Card';
 import classes from '../components/AddUser.module.css';
 import Button from '../components/Button';
 import { useState } from 'react';
-
+import ErrorModal from '../components/ErrorModal';
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {    //앞뒤 공백을 제거
+            setError({
+                title: 'Invalid input',
+                message: 'please enter a valid name and age ( non-empty values).'
+            });
         return;
         }
         if (+enteredAge < 1){       //위에 초기화는 문자열로 하고있지만 정수형태로 받아오는것으로 만들기 위해 +기호를 사용하여 정수로 바꾸는 안전한 작성법(기교임)
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid age ( >0 ).'
+            });
             return;
         }
         props.onAddUser(enteredUsername,enteredAge);
@@ -27,8 +36,12 @@ const AddUser = (props) => {
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value);
     };
-
-    return (
+        const errorHandler = () => {
+            setError(null)
+        };
+    return ( 
+        <div>
+        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />} 
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor='username'>UserName</label>
@@ -38,6 +51,7 @@ const AddUser = (props) => {
             <Button type='submit'>Add User</Button> 
         </form>
         </Card>
+        </div>
     );
 };
 export default AddUser; 
